82a81de8f88af7a8e23c76fc2d5be7a6f77e2480
Thierry Chevallier <tx.chevallier@akka.eu>
2013-03-07 11:36:30 +0100

    DB migration for geosource v290 (config-overrides)

---
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/290/migrate-db-oracle.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Services
+  (
+    id         int,
+    name       varchar2(64)   not null,
+	class       varchar2(1048)   not null,
+    description       varchar2(1048),
+        
+    primary key(id)
+  );
+  
+CREATE TABLE ServiceParameters
+  (
+    id         int,
+    service	 	int,
+    name       varchar2(64)   not null,
+    value       varchar2(1048)   not null,
+    
+    primary key(id),
+        
+    foreign key(service) references Services(id)
+  );
