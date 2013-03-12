5dc84fa21249088d0b210a2ffbe04f90899eca15
François Prunayre <fxprunayre@gmail.com>
2013-03-12 07:42:47 +0100

    Add SQL migration from 2.7.3 to 2.9.0.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/290/migrate-db-oracle.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/290/migrate-db-oracle.sql
@@ -1,3 +1,18 @@
+-- Spring security
+ALTER TABLE Users ADD security varchar2(128) default '';
+ALTER TABLE Users ADD authtype varchar2(32);
+ALTER TABLE Users MODIFY password varchar2(120);
+
+-- Support multiple profiles per user
+ALTER TABLE usergroups ADD profile varchar2(32);
+UPDATE usergroups SET profile = (SELECT profile from users WHERE id = userid);
+ALTER TABLE usergroups DROP PRIMARY KEY;
+ALTER TABLE usergroups ADD PRIMARY KEY (userid, profile, groupid);
+
+ALTER TABLE Metadata MODIFY harvestUri varchar2(512);
+
+ALTER TABLE HarvestHistory ADD elapsedTime int;
+
 CREATE TABLE Services
   (
     id         int,
@@ -19,5 +34,3 @@ CREATE TABLE ServiceParameters
         
     foreign key(service) references Services(id)
   );
-  
-  UPDATE Settings SET value='2.9.0' WHERE name='version';
--- web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/290/migrate-db-postgis.sql
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/290/migrate-db-postgis.sql
@@ -1,3 +1,18 @@
+-- Spring security
+ALTER TABLE Users ADD security varchar(128) default '';
+ALTER TABLE Users ADD authtype varchar(32);
+ALTER TABLE users ALTER "password" TYPE character varying(120);
+
+-- Support multiple profiles per user
+ALTER TABLE usergroups ADD profile varchar(32);
+UPDATE usergroups SET profile = (SELECT profile from users WHERE id = userid);
+ALTER TABLE usergroups DROP CONSTRAINT usergroups_pkey;
+ALTER TABLE usergroups ADD PRIMARY KEY (userid, profile, groupid);
+
+ALTER TABLE Metadata ALTER harvestUri TYPE varchar(512);
+
+ALTER TABLE HarvestHistory ADD elapsedTime int;
+
 CREATE TABLE Services
   (
     id         int,
@@ -19,5 +34,3 @@ CREATE TABLE ServiceParameters
         
     foreign key(service) references Services(id)
   );
-  
-  UPDATE Settings SET value='2.9.0' WHERE name='version';
