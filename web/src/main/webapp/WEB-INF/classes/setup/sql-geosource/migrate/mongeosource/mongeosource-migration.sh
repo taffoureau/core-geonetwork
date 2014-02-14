474f81e0782847672fa3883ffa17a21f7b7e5682
François Prunayre <fxprunayre@gmail.com>
2014-02-14 08:27:36 +0100

    Add password for mongeosource migration script config.

--- web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/mongeosource/mongeosource-migration.sh
+++ web/src/main/webapp/WEB-INF/classes/setup/sql-geosource/migrate/mongeosource/mongeosource-migration.sh
@@ -3,10 +3,10 @@
 function showUsage
 {
   echo -e "\nThis script is used to migrate mongeosource from 2.7.3 to 2.11.0"
-  echo -e "Usage: ./`basename $0 $1` username database server port"
+  echo -e "Usage: ./`basename $0 $1` username database server port password"
 }
 
-if [ $# -lt 2 ]
+if [ $# -lt 4 ]
 then
   showUsage
   exit
@@ -19,6 +19,7 @@ then
 fi
 
 # TODO : default to 127.0.0.1 and port 5432
+export PGPASSWORD=$5
 psql -U $1 -h $3 -p $4 -f run.sql $2 -L log-$2-query.log -o log-$2.log
 
 
